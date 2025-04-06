ad84b8f6ab8d4fda34e7e1b152705cbeb12b464a
Okunola Olamide <okunolaolubanjo@gmail.com>
2025-04-06 13:56:38 +0100

    Woked on the side Menu and the sidemenu responsive

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
+import Header from './components/Header/Header'
 import SideMenu from './components/SideMenu/SideMenu'
 
 const App = () => {
   return (
-    <div className='bg-[#1D1932] opacity-0min-h-screen flex'>
+    <div className='bg-[#1D1932] min-h-screen flex'>
       <SideMenu />
       
       <div className="bg-[#1D1932] flex-1 text-white p-4">
-        {/* Your main page content or routes go here */}
-        <h1>Welcome to the App!</h1>
+        <Header />
+
       </div>
     </div>
   )
--- my-react-app/src/components/SideMenu/SideMenu.tsx
+++ my-react-app/src/components/SideMenu/SideMenu.tsx
@@ -1,12 +1,18 @@
-
 import IMAGES from "../../constants/image";
 
 const SideMenu = () => {
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col justify-between w-16 sw-20 md:w-24 lg:w-28">
-        <div className="flex flex-col items-center gap-6">
-          <img src={IMAGES.logo}  />
+    <div className="flex h-screen relative">
+      {/* Sidebar */}
+      <div className="flex flex-col justify-between w-16 sw-20 md:w-24 lg:w-28 py-6 bg-[#1D1932]">
+        
+        {/* Logo at Top */}
+        <div className="flex justify-center mb-6">
+          <img src={IMAGES.logo} alt="Logo" />
+        </div>
+
+        {/* Desktop Vertical Icons */}
+        <div className="hidden md:flex flex-col items-center gap-6">
           <img src={IMAGES.dashboard} alt="Dashboard" />
           <img src={IMAGES.bid} alt="Bid" />
           <img src={IMAGES.heart} alt="Heart" />
@@ -14,10 +20,21 @@ const SideMenu = () => {
           <img src={IMAGES.profile} alt="Profile" />
           <img src={IMAGES.setting} alt="Setting" />
         </div>
-        
-        <div className="flex justify-center">
-          <img src={IMAGES.signOut} />
+
+        {/* Sign Out Button (Desktop Only) */}
+        <div className="hidden md:flex justify-center mt-6">
+          <img src={IMAGES.signOut} alt="Sign-out" />
         </div>
+      </div>
+
+      <div className="md:hidden fixed bottom-0 left-0 w-full bg-[#1D1932] flex justify-around items-center py-3 z-50">
+        <img src={IMAGES.dashboard} alt="Dashboard" className="w-6 h-6" />
+        <img src={IMAGES.bid} alt="Bid" className="w-6 h-6" />
+        <img src={IMAGES.heart} alt="Heart" className="w-6 h-6" />
+        <img src={IMAGES.collection} alt="Collection" className="w-6 h-6" />
+        <img src={IMAGES.profile} alt="Profile" className="w-6 h-6" />
+        <img src={IMAGES.setting} alt="Setting" className="w-6 h-6" />
+        <img src={IMAGES.signOut} alt="Sign-out" className="w-6 h-6" />
       </div>
 
       {/* Main Content */}
